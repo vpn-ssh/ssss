3783b282eac4b63c063425a3c77c3789f3252f80
CoutySSH <100492902+vpn-ssh@users.noreply.github.com>
2023-07-05 19:22:13 -0300

    Update sicronizar.sh

--- sicronizar.sh
+++ sicronizar.sh
@@ -3,4 +3,7 @@ wget https://raw.githubusercontent.com/vpn-ssh/ssss/main/remove.sh
 chmod 777 remove.sh
 wget https://raw.githubusercontent.com/vpn-ssh/ssss/main/AtlantusMakeAccount.sh
 chmod 777 AtlantusMakeAccount.sh
+clear
 echo -e " \e[0;31mInstalado com Sucesso \e[0m"
+sleep 10
+menu
